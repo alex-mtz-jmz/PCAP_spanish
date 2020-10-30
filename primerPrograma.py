6553d527d3fccab06cf07ee90453785fc16c370b
alejandro.martinez <alejandro.martinezj@dominion.mx>
2020-10-30 17:21:08 -0600

    Alta de proyecto de programación básica de Python

---
+++ primerPrograma.py
@@ -0,0 +1,77 @@
+from builtins import print, property
+
+print("Hissssssssssssssssssssss...")
+
+print("Hola!")
+print("¡Bienvenido a Fundamentos de Programación en Python!")
+print("ESTO ES EL MODO SANDBOX.")
+
+# Laboratorio 2.1.1.7
+
+print("Hola mundo")
+print("mi nombre es Alejandro Martinez")
+
+# 2.1.1.19 LABORATORIO: La función print()
+
+print("Fundamentos", "Programación", "en", sep="***", end="...")
+print("Python")
+
+# 2.1.1.20 LABORATORIO: Dando formato a la salida
+
+print("          ^          " * 2)
+print("         / \\         " * 2)
+print("        /   \\        " * 2)
+print("       /     \\       " * 2)
+print("      /       \\      " * 2)
+print("     /         \\     " * 2)
+print("    /           \\    " * 2)
+print("   /             \\   " * 2)
+print("  /               \\  " * 2)
+print(" <----+       +----> " * 2)
+print("      |       |      " * 2)
+print("      |       |      " * 2)
+print("      |       |      " * 2)
+print("      |       |      " * 2)
+print("      |       |      " * 2)
+print("      +-------+      " * 2)
+
+print(True > False)
+print(True < False)
+
+# 2.1.2.11 LABORATORIO: Literales de Python - cadenas
+
+print('"Estoy"', '""Aprendiendo""', '"""Python"""', sep="\n")
+
+# Literales
+print("ola ke ase")
+print(1234)
+
+# cadenas
+print("ola ke ase")
+
+# Notacion cientifica
+print(3e14)
+print(3.141592e-3)
+
+# enteros positivos y negativos
+print(-123_456)
+print(+345_320)
+
+# separacion de enteros para hacer mas legible la lectura del numero
+print(111_111_111)
+
+# flotantes
+print(4.)
+print(.4)
+
+# binarios, octales y hexadecimales
+print(0b111)
+print(0o123)
+print(0x123)
+print(123)
+
+# boleanos
+print(True > False)
+print(True < False)
+
+# operadores
